32bfcc1669e17cb911b1ffdc872574ad6876c9cf
obertobr <real.azzi@gmail.com>
2024-11-25 01:41:34 -0300

    checking members in the role added on deletion

--- src/Service/Implemetation/Institution/role.crud.service.impl.ts
+++ src/Service/Implemetation/Institution/role.crud.service.impl.ts
@@ -5,6 +5,7 @@ import ErrorBuilder from "src/Service/Validation/error.builder";
 import Role from "src/Model/Institution/role.entity";
 import RoleCrudServiceInterface from "src/Service/Interface/Institution/role.crud.service.interface";
 import RoleCrudRepositoryInterface from "src/Repository/Interface/Institution/role.crud.repository.interface";
+import ValidationExcpection from "src/Service/Validation/validation.exception";
 
 @Injectable()
 export default class RoleCrudServiceImpl extends BaseCrudService<Role> implements RoleCrudServiceInterface {
@@ -21,6 +22,20 @@ export default class RoleCrudServiceImpl extends BaseCrudService<Role> implement
         }
 
         return errorBuilder;
+    }
+
+    async delete(id: number): Promise<void> {
+    
+        const entity = await this.repository.getById(id, ["memberList"])
+        
+        if (!entity) {
+            throw new ValidationExcpection([`Entidade com o ID: ${id} não encontrada ou já deletada`],'Erro ao deletar cargo');
+        }
+        if(entity.memberList.length != 0){
+            throw new ValidationExcpection([`Existem membros usando esse cargo`],'Erro ao deletar cargo');
+        }
+        
+        await this.repository.delete(id);
     }
    
 
